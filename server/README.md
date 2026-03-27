b107d50901072913c9455028bcb13400bbaabbb1
Martin Algreen <git@mal.green>
2026-03-27 10:24:26 +0100

    server: add tokio & axum + setup router

--- server/README.md
+++ server/README.md
@@ -1,1 +1,4 @@
 # Server
+
+This package serves a website and an API backend.
+It uses `axum` for the HTTP routing, and `maud` for the HTML rendering.
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:9876").await.unwrap();
+
+    let router = Router::new()
+        .route("/", get("Hello!"))
+        .route("/goodbye", get("Goodbye!"));
+
+    axum::serve(listener, router).await.unwrap();
 }
