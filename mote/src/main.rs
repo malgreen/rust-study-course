1dc93097c538b69dddb8c7b9c5312c2f831c3d98
Martin Algreen <git@mal.green>
2026-03-30 12:20:27 +0200

    add wifi module and use it

--- mote/src/main.rs
+++ mote/src/main.rs
@@ -7,11 +7,17 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
+use crate::wifi::{build_networking_stack, connect_wifi, setup_tcp, setup_wifi};
+use alloc::vec::Vec;
 use defmt::{error, info};
+use embedded_io::{Read, Write};
 use esp_hal::clock::CpuClock;
 use esp_hal::main;
 use esp_hal::time::{Duration, Instant};
 use esp_hal::timer::timg::TimerGroup;
+use esp_println::print;
+use smoltcp::wire::IpAddress;
+
 use {esp_backtrace as _, esp_println as _};
 
 extern crate alloc;
@@ -27,8 +33,8 @@ esp_bootloader_esp_idf::esp_app_desc!();
 )]
 #[main]
 fn main() -> ! {
-    // generator version: 1.2.0
-
+    // === device setup === //
+    // TODO: configure clock
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
@@ -37,19 +43,61 @@ fn main() -> ! {
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_rtos::start(timg0.timer0);
 
-    let wifi_f = match wifi::init(peripherals.WIFI) {
-        Ok(v) => v,
-        Err(e) => {
-            error!("WiFi initialization error: {}", e);
-            panic!()
-        }
-    };
+    // === networking setup === //
+    let radio_controller: esp_radio::Controller<'static> = esp_radio::init().unwrap();
 
+    let (mut wifi_controller, mut wifi_device) =
+        setup_wifi(&radio_controller, peripherals.WIFI).unwrap();
+
+    let (tcp_interface, mut tcp_sockets) = setup_tcp(&mut wifi_device);
+
+    let net_stack = build_networking_stack(wifi_device, tcp_interface, &mut tcp_sockets);
+
+    if let Some(e) = connect_wifi(&mut wifi_controller) {
+        error!("WiFi connection error: {}", e);
+        panic!();
+    }
+
+    // === get IP address for server === //
+    let server_ip_parts: Vec<u8> = env!("SERVER_IP")
+        .split(".")
+        .map(|p| p.parse::<u8>().unwrap())
+        .collect();
+    assert!(
+        server_ip_parts.len() == 4,
+        "Server IP must be a valid IPv4 address"
+    );
+    let server_ip: IpAddress = IpAddress::v4(
+        server_ip_parts[0],
+        server_ip_parts[1],
+        server_ip_parts[2],
+        server_ip_parts[3],
+    );
+    let server_port: u16 = env!("SERVER_PORT").parse().unwrap();
+
+    // === send http request === //
+    let mut tcp_write_buffer = [0u8; 2048];
+    let mut tcp_read_buffer = [0u8; 2048];
+    let mut tcp_socket = net_stack.get_socket(&mut tcp_read_buffer, &mut tcp_write_buffer);
+    tcp_socket.work();
+    tcp_socket.open(server_ip, server_port).unwrap();
     loop {
+        tcp_socket.write(b"GET / HTTP/1.0").unwrap(); // TODO
+        tcp_socket.flush().unwrap();
+
+        // === listen for http response === //
+        let mut tcp_socket_buffer = [0u8; 512];
+        while let Ok(len) = tcp_socket.read(&mut tcp_socket_buffer) {
+            let part = unsafe { core::str::from_utf8_unchecked(&tcp_socket_buffer[..len]) };
+            print!("{part}");
+            tcp_socket.work();
+        }
+
         info!("Hello world!");
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
     }
+    // tcp_socket.disconnect();
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples
 }
--- mote/src/wifi.rs
+++ mote/src/wifi.rs
@@ -1,38 +1,191 @@
-use core::{
-    error::{self, Error},
-    fmt,
+use blocking_network_stack::Stack;
+use defmt::info;
+use esp_hal::{peripherals::WIFI, rng::Rng};
+use esp_radio::wifi::{
+    ClientConfig, ModeConfig, ScanConfig, WifiController, WifiDevice, WifiError,
 };
+use smoltcp::iface::{SocketSet, SocketStorage};
 
-use alloc::boxed::Box;
-use defmt::Format;
-use esp_hal::peripherals::{Peripherals, WIFI};
-use esp_radio::{InitializationError, wifi::WifiError};
+const WIFI_SSID: &str = env!("MOTE_WIFI_SSID");
+const WIFI_PASSWORD: &str = env!("MOTE_WIFI_PASSWORD");
 
-//
-pub fn init(wifi_peripheral: WIFI) -> Result<&'static str, EspWifiError> {
-    let radio_controller = esp_radio::init()?;
+// struct WifiBundle<'a> {
+//     wifi_controller: WifiController<'a>,
+//     stack: Stack<'a, WifiDevice<'a>>,
+// }
 
+pub fn setup_wifi<'a>(
+    radio_controller: &'a esp_radio::Controller<'a>,
+    wifi_peripheral: WIFI<'a>,
+) -> Result<(WifiController<'a>, WifiDevice<'a>), WifiError> {
+    let (wifi_controller, wifi_interfaces) =
+        esp_radio::wifi::new(&radio_controller, wifi_peripheral, Default::default())?;
+
+    let wifi_device = wifi_interfaces.sta;
+    Ok((wifi_controller, wifi_device))
+}
+
+pub fn setup_tcp<'a>(
+    wifi_device: &mut WifiDevice,
+) -> (smoltcp::iface::Interface, [SocketStorage<'a>; 3]) {
+    (
+        smoltcp::iface::Interface::new(
+            smoltcp::iface::Config::new(smoltcp::wire::HardwareAddress::Ethernet(
+                smoltcp::wire::EthernetAddress::from_bytes(&wifi_device.mac_address()),
+            )),
+            wifi_device,
+            smoltcp::time::Instant::from_micros(
+                esp_hal::time::Instant::now()
+                    .duration_since_epoch()
+                    .as_micros() as i64,
+            ),
+        ),
+        Default::default(),
+    )
+}
+
+pub fn build_networking_stack<'a>(
+    wifi_device: WifiDevice<'a>,
+    tcp_interface: smoltcp::iface::Interface,
+    tcp_sockets: &'a mut [SocketStorage<'a>],
+) -> Stack<'a, WifiDevice<'a>> {
+    let mut socket_set = SocketSet::new(&mut tcp_sockets[..]);
+
+    let mut dhcp_socket = smoltcp::socket::dhcpv4::Socket::new();
+    dhcp_socket.set_outgoing_options(&[smoltcp::wire::DhcpOption {
+        kind: 12,
+        data: b"co2Mote",
+    }]);
+    socket_set.add(dhcp_socket);
+
+    let rng = Rng::new();
+    let now = || {
+        esp_hal::time::Instant::now()
+            .duration_since_epoch()
+            .as_millis()
+    }; // zero-arg closure, I think...
+
+    Stack::new(tcp_interface, wifi_device, socket_set, now, rng.random())
+}
+
+/// Connects to WiFi using SSID and Password set as environment variables
+// TODO: use Result?
+pub fn connect_wifi(wifi_controller: &mut WifiController) -> Option<WifiError> {
+    wifi_controller
+        .set_power_saving(esp_radio::wifi::PowerSaveMode::None)
+        .ok()?;
+
+    let client_config = ModeConfig::Client(
+        ClientConfig::default()
+            .with_ssid(WIFI_SSID.into())
+            .with_password(WIFI_PASSWORD.into()),
+    );
+
+    wifi_controller.set_config(&client_config).ok()?;
+    wifi_controller.start().ok()?;
+
+    info!("WiFi controller scanning...");
+    let scan_config = ScanConfig::default().with_max(10);
+    wifi_controller.scan_with_config(scan_config).ok()?;
+
+    info!("WiFi controller connecting...");
+    wifi_controller.connect().ok()?;
+    while !wifi_controller.is_connected().ok()? {}
+    return None;
+}
+
+/* TODO: det ville måske være rart at have det hele i en funktion,
+ * men det er lidt svært at få lifetimes til at fungere som jeg
+ * vil have det
+
+// much of this is based on https://esp32.implrust.com/wifi/sta-mode-access-website.html
+// pub fn init<'a>(wifi_peripheral: WIFI<'a>) -> Result<Stack<'a, WifiDevice<'a>>, WifiBundleError> {
+pub fn init<'a>(wifi_peripheral: WIFI<'a>) -> Result<WifiBundle<'a>, WifiBundleError> {
+    let radio_controller: esp_radio::Controller<'a> = esp_radio::init()?;
     let (mut wifi_controller, interfaces) =
         esp_radio::wifi::new(&radio_controller, wifi_peripheral, Default::default())?;
 
-    return Ok("asd");
+    let mut device = interfaces.sta;
+
+    let tcp_interface = smoltcp::iface::Interface::new(
+        smoltcp::iface::Config::new(smoltcp::wire::HardwareAddress::Ethernet(
+            smoltcp::wire::EthernetAddress::from_bytes(&device.mac_address()),
+        )),
+        &mut device,
+        smoltcp::time::Instant::from_micros(
+            esp_hal::time::Instant::now()
+                .duration_since_epoch()
+                .as_micros() as i64,
+        ),
+    );
+
+    let mut socket_set_entries: [SocketStorage; 3] = Default::default();
+    let mut socket_set = SocketSet::new(&mut socket_set_entries[..]);
+
+    let mut dhcp_socket = smoltcp::socket::dhcpv4::Socket::new();
+    dhcp_socket.set_outgoing_options(&[smoltcp::wire::DhcpOption {
+        kind: 12,
+        data: b"co2Mote",
+    }]);
+    socket_set.add(dhcp_socket);
+
+    let rng = Rng::new();
+    let now = || {
+        esp_hal::time::Instant::now()
+            .duration_since_epoch()
+            .as_millis()
+    }; // zero-arg closure (call with `now();`)
+
+    let stack = Stack::new(tcp_interface, device, socket_set, now, rng.random());
+
+    wifi_controller.set_power_saving(esp_radio::wifi::PowerSaveMode::None)?;
+
+    let client_config = ModeConfig::Client(
+        ClientConfig::default()
+            .with_ssid(WIFI_SSID.into())
+            .with_password(WIFI_PASSWORD.into()),
+    );
+
+    wifi_controller.set_config(&client_config)?;
+    wifi_controller.start()?;
+
+    info!("WiFi controller scanning...");
+    let scan_config = ScanConfig::default().with_max(10);
+    wifi_controller.scan_with_config(scan_config)?;
+
+    info!("WiFi controller connecting...");
+    wifi_controller.connect()?;
+    while !wifi_controller.is_connected()? {}
+
+    info!("WiFi controller connected, waiting for IP address...");
+    while !stack.is_iface_up() {
+        stack.work();
+    }
+
+    Ok(WifiBundle {
+        wifi_controller: wifi_controller,
+        stack: stack,
+    })
+    // Ok(stack)
 }
 
 // error stuff
 #[derive(Format)]
-pub enum EspWifiError {
+pub enum WifiBundleError {
     WifiError(WifiError),
     InitializationError(InitializationError),
 }
 // we need to implement From<T> because it allows use to use the `?` operator,
-impl From<WifiError> for EspWifiError {
+impl From<WifiError> for WifiBundleError {
     fn from(value: WifiError) -> Self {
-        EspWifiError::WifiError(value)
+        WifiBundleError::WifiError(value)
     }
 }
 
-impl From<InitializationError> for EspWifiError {
+impl From<InitializationError> for WifiBundleError {
     fn from(value: InitializationError) -> Self {
-        EspWifiError::InitializationError(value)
+        WifiBundleError::InitializationError(value)
     }
 }
+
+*/
